104c1fc26105e957064ecdf84f9c71b2b0bef83f
ko2ic <ko2ic.dev@gmail.com>
2026-03-16 01:19:56 +0900

    change select pane color

--- .claude/reset-highlight.sh
+++ .claude/reset-highlight.sh
@@ -2,6 +2,7 @@
 if [ -n "$TMUX_PANE" ]; then
   WIN=$(tmux display-message -t "$TMUX_PANE" -p '#I')
   tmux set-window-option -t ":$WIN" window-status-style default
+  tmux set-window-option -t ":$WIN" window-status-current-style 'fg=white,bg=blue,bold'
   tmux set-option -p -t "$TMUX_PANE" window-style 'bg=default'
   tmux set-option -p -t "$TMUX_PANE" window-active-style 'bg=default'
 fi
